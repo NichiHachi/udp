056de8ef42efd0222acba7734b93b8f7621d892e
Eclairsombre <alexandre.thny@gmail.com>
2025-03-25 17:13:51 +0100

    init client

---
+++ app/src/main/java/udp/Client.java
@@ -0,0 +1,39 @@
+package udp;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Client {
+    private final InetAddress serverAddress;
+    private final int serverPort;
+    private final DatagramSocket clientSocket;
+
+    public Client(String serverIp, int serverPort) throws Exception {
+        this.serverAddress = InetAddress.getByName(serverIp);
+        this.serverPort = serverPort;
+        this.clientSocket = new DatagramSocket();
+    }
+
+    public void sendMessage(String message) throws Exception {
+        byte[] sendData = message.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+        clientSocket.send(sendPacket);
+    }
+
+    public String receiveMessage() throws Exception {
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        return new String(receivePacket.getData(), 0, receivePacket.getLength());
+    }
+
+    public void close() {
+        clientSocket.close();
+    }
+
+    public void connect() throws Exception {
+        sendMessage("hello serveur RX302");
+        System.out.println("Serveur RX302 ready :  " + receiveMessage());
+    }
+}
