91e02b8f44698f7d128d0b144a2f72665781a278
Eclairsombre <alexandre.thny@gmail.com>
2025-03-25 17:17:46 +0100

    test client

--- app/src/main/java/udp/App.java
+++ app/src/main/java/udp/App.java
@@ -2,11 +2,14 @@
 package udp;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        try {
+            Client client = new Client("127.0.0.1", 5000);
+            client.connect();
+            client.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
