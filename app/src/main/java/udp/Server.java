f2f996b53780072138f30dc43e79ee40d756655b
Nichi Hachi <kevin.angelier@aol.fr>
2025-03-28 09:49:10 +0100

    feature(Server/Client): Put the main function inside of classes

--- app/src/main/java/udp/Server.java
+++ app/src/main/java/udp/Server.java
@@ -51,4 +51,12 @@ public class Server {
             }
         }
     }
+
+    public static void main(String[] args) {
+        int port = 9876;
+
+        System.out.println("Starting UDP server on port " + port);
+        Server server = new Server(port);
+        server.run();
+    }
 }
